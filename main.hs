ae188c37afe73c176d5c97691c1e3f3cff2c4647
Lida Hixson <lahixson@live.unc.edu>
2019-01-31 18:09:54 +0000

    quiz

--- main.hs
+++ main.hs
@@ -2,4 +2,3 @@ finalGrade :: [Integer] -> [Integer] -> Integer
 finalGrade xs ys = (sum (zipWith (*) xs ys)) `div` (sum (ys))
 
 --[x*y | x <- [1..10],y<-[1..10]]
---(sum (zipWith (*) grade weight)) `div` (sum (weight))
